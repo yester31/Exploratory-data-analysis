bc2dff074135efb00d61c9687c03bcc73bdeeeee
yhpark <yester31@naver.com>
2022-06-21 22:36:13 +0900

    add bar cahrt box cnt by class & by images

--- DatasetAnalysis.py
+++ DatasetAnalysis.py
@@ -3,6 +3,8 @@ import os
 import random
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
 
 color_map = [(244, 67, 54), (233, 30, 99), (156, 39, 176), (103, 58, 183), (63, 81, 181), (33, 150, 243), (3, 169, 244),
              (0, 188, 212), (0, 150, 136), (76, 175, 80)]
@@ -21,23 +23,29 @@ class CocoAnalysis:
                 self.categories = coco['categories']
 
                 print('Total images: {}\nTotal boxes: {}\nTotal classes: {}'.format(len(self.images), len(self.annotations), len(self.categories)))
-                self.class_names = [str(cate['id']) + ' : ' + cate['name'] for cate in self.categories]
-                print('Class names : {} '.format(self.class_names))
+                self.class_names = [cate['name'] for cate in self.categories]
 
+                self.dic_imgid_anno_cnt = {}
                 self.dic_imgid_annos = {}
                 self.dic_imgid_img = {}
-                for img in self.images:
+                for img in tqdm(self.images):
+                    self.dic_imgid_anno_cnt[img['id']] = 0
                     self.dic_imgid_img[img['id']] = img
                     self.dic_imgid_annos[img['id']] = []
 
-                self.dic_annoid_anno = {}
-                for anno in self.annotations:
-                    self.dic_imgid_annos[anno['image_id']].append(anno)
-                    self.dic_annoid_anno[anno['id']] = anno
-
                 self.dic_cateid_cate = {}
-                for cate in self.categories:
+                for cate in tqdm(self.categories):
                     self.dic_cateid_cate[cate['id']] = cate
+
+                self.boxes_by_class = np.zeros(len(self.categories))
+                self.boxes_by_image = np.zeros(len(self.images))
+                for anno in tqdm(self.annotations):
+                    self.dic_imgid_anno_cnt[anno['image_id']] += 1
+                    self.dic_imgid_annos[anno['image_id']].append(anno)
+                    self.boxes_by_class[anno['category_id']] += 1
+                    self.boxes_by_image[anno['image_id']] += 1
+
+
 
         else:  # anno_path 파일이 없다면
             print('Check the annotation file path!!!')
@@ -105,10 +113,38 @@ class CocoAnalysis:
         plt.imshow(image[:, :, ::-1])
         plt.show()
 
+    # 이미지 별로 바운딩 박스 개수를 박스 차트로 출력
+    def show_bar_chart_box_by_image(self):
+        image_list = self.dic_imgid_anno_cnt.items()
+        x, y = zip(*image_list)
+        plt.figure(figsize=(15, 10))
+        plt.bar(np.arange(len(self.images)), y, width=1)
+        plt.yticks(np.arange(10) * max(self.dic_imgid_anno_cnt.values()) / 10, np.arange(10) * int(max(self.dic_imgid_anno_cnt.values()) / 10), fontsize=20)
+        plt.xticks(np.arange(10) * int(len(self.images) / 10), np.arange(10) * int(len(self.images) / 10), fontsize=15)
+        plt.ylabel('The number of box', fontsize=15)
+        plt.xlabel('Image', fontsize=15)
+        plt.title('Distribution of boxes per image', fontsize=20)
+        plt.show()
+
+    # 클래스 별로 바운딩 박스 개수를 박스 차트로 출력
+    def show_bar_chart_box_by_class(self):
+        plt.figure(figsize=(len(self.class_names), 10))
+        plt.bar(self.class_names, self.boxes_by_class, width=0.8)
+        plt.yticks(np.arange(10) * self.boxes_by_class.max() / 10, np.arange(10) * int(self.boxes_by_class.max() / 10), fontsize=15)
+        plt.xticks(np.arange(len(self.class_names)), self.class_names, fontsize=15, rotation=35, ha='right')
+        plt.ylabel('The number of box', fontsize=15)
+        plt.xlabel('class', fontsize=15)
+        plt.title('Distribution of boxes per class', fontsize=20)
+        for idx, class_cnt in enumerate(self.boxes_by_class):
+            plt.text(idx, class_cnt, str(round(class_cnt / sum(self.boxes_by_class) * 100, 2)) + '%', fontsize=15, color='blue', horizontalalignment='center', verticalalignment='bottom')
+        plt.show()
+
 
 if __name__ == "__main__":
     anno_path = 'Self Driving Car.v3-fixed-small.coco/export/_annotations.coco.json'
     img_dir_path = 'Self Driving Car.v3-fixed-small.coco/export/'
     coco = CocoAnalysis(anno_path, img_dir_path)
     #coco.show_random_img_class()
-    coco.show_img_id()
+    #coco.show_img_id()
+    #coco.show_bar_chart_box_by_class()
+    coco.show_bar_chart_box_by_image()
--- main.py
+++ main.py
@@ -11,8 +11,16 @@ if __name__ == "__main__":
     # 객체 생성
     coco = CocoAnalysis(anno_path, img_dir_path)
 
+    # 이미지 표현
+    # 랜덤 출력
     coco.show_random_img_all()
-
+    # 램덤 출력 & 특정 클래스만 바운딩 박스 표시
     coco.show_random_img_class(2)
-
+    # 특정 이미지 아이디의 이미지 출력
     coco.show_img_id(0)
+
+    # 차트 표현
+    # 클래스 별 바운딩 박스의 개수를 바 차트로 출력
+    coco.show_bar_chart_box_by_class()
+    # 이미지 별로 바운딩 박스 개수를 박스 차트로 출력
+    coco.show_bar_chart_box_by_image()
